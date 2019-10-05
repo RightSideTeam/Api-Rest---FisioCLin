780a8e55936da9bb4c9cddac5b6cad79ccc98656
Matheus Lima <matheusldasilva20088@gmail.com>
2019-10-04 22:13:01 -0300

    api rest

--- src/main/java/com/rightside/fisioclinapi/FisioclinapiApplication.java
+++ src/main/java/com/rightside/fisioclinapi/FisioclinapiApplication.java
@@ -14,29 +14,6 @@ public class FisioclinapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FisioclinapiApplication.class, args);
 		
-		Consulta consulta = new Consulta();
-		Horario horario = new Horario();
-		horario.setData("01/12/2020");
-		horario.setDiaSemana("Quinta-feira");
-		horario.setHora("23:30");
-		horario.setId(1);
-		Paciente paciente = new Paciente();
-		paciente.setDataDeNascimento("01/12/1994");
-		paciente.setEmail("matheusldasilva20088@gmail.com");
-		paciente.setId(1);
-		paciente.setSobrenome("lima");
-		paciente.setTelefone("32991313947");
-		paciente.setUrlFoto("www.google.com");
-		
-		
-		consulta.setDescricao("Consulta de revisão, dor no joelho");
-		consulta.setHorario(horario);
-		consulta.setId(1);
-		consulta.setPaciente(paciente);
-		Gson gson = new Gson();
-		String json = gson.toJson(consulta);
-		
-		System.out.println(json);
 		
 	}
 
